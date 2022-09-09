23d5b0ce508c76d69937eb6c78f52e3abd75c392
DimasEly <110694492+DimasEly@users.noreply.github.com>
2022-09-08 22:10:05 -0300

    Filme Service criado

--- app/src/main/java/br/com/alura/appfilmes/webclient/services/FilmeService.kt
+++ app/src/main/java/br/com/alura/appfilmes/webclient/services/FilmeService.kt
@@ -1,4 +1,10 @@
 package br.com.alura.appfilmes.webclient.services
 
+import br.com.alura.appfilmes.FilmeResposta
+import retrofit2.Call
+import retrofit2.http.GET
+
 interface FilmeService {
+    @GET ("movie/popular")
+    suspend fun buscaTodas(): Call<FilmeResposta>
 }
