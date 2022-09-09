3b00d5a26e2ef757dd3d8aa4e49d6d04300a3e47
DimasEly <110694492+DimasEly@users.noreply.github.com>
2022-09-08 22:01:44 -0300

    filme resposta

---
+++ app/src/main/java/br/com/alura/appfilmes/FilmeResposta.kt
@@ -0,0 +1,11 @@
+package br.com.alura.appfilmes
+
+import br.com.alura.appfilmes.webclient.model.Filme
+import com.google.gson.annotations.SerializedName
+
+data class FilmeResposta (
+    @SerializedName("results") val resultado: List<Filme>,
+    //@SerializedName("page") val pagina: Int,
+    @SerializedName("total_pages") val total_paginas: Int,
+    @SerializedName("total_results") val total_resultados: Int
+        )
