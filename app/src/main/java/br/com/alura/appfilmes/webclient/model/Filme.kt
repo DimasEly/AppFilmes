30ce7f302b346ddacbf059d5380eb204586c4eb9
DimasEly <110694492+DimasEly@users.noreply.github.com>
2022-09-08 22:16:22 -0300

    Filme criado

--- app/src/main/java/br/com/alura/appfilmes/webclient/model/Filme.kt
+++ app/src/main/java/br/com/alura/appfilmes/webclient/model/Filme.kt
@@ -1,5 +1,11 @@
 package br.com.alura.appfilmes.webclient.model
 
-class Filme (
+import com.google.gson.annotations.SerializedName
 
+class Filme (
+        @SerializedName("id") val id: Int,
+        @SerializedName("tittle") val titulo: String,
+        @SerializedName("release_data") val dataLancamento: String,
+        @SerializedName("original_lenguage") val linguagem: String,
+        @SerializedName("poster_path") val imagemVertical: String
         )
